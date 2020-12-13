8022db44863392ddd5e00beb8c621f8f31e336dd
dreus <leloqu@icloud.com>
2020-12-12 23:37:51 -0500

    se cambio ruta de heroku

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -16,7 +16,7 @@ export default {
   created: function() {
     this.username = this.$route.params.username
     let self = this
-    axios.get("https://cajero-api.herokuapp.com/user/balance/" + this.username).then((result) => {
+    axios.get("https://cajero-api4.herokuapp.com/user/balance/" + this.username).then((result) => {
       self.balance = result.data.balance
     }).catch((error) => {
       alert("ERROR Servidor")
