c30005744b7e863db45e45ca705b1e30eefdaeb3
dreus <leloqu@icloud.com>
2020-12-12 23:25:26 -0500

    se cambio la ruta de enlace a heroku

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -16,7 +16,7 @@ export default {
   created: function() {
     this.username = this.$route.params.username
     let self = this
-    axios.get('http://127.0.0.1:8000/user/balance/' + this.username).then((result) => {
+    axios.get("https://cajero-api.herokuapp.com/user/balance/" + this.username).then((result) => {
       self.balance = result.data.balance
     }).catch((error) => {
       alert("ERROR Servidor")
